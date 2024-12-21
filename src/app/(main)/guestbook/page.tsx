93c7b606146164b6211af0d985efda3b608ec3c8
NeilRow56 <neilrowland56@gmail.com>
2024-12-21 06:37:09 +0000

    Tip Tap PostEditor upto being able to submit posts - no loading state. Entries made in database

--- src/app/(main)/guestbook/page.tsx
+++ src/app/(main)/guestbook/page.tsx
@@ -5,7 +5,7 @@ const GuestbookRoute = async () => {
   const { getUser } = getKindeServerSession()
   const user = await getUser()
   return (
-    <main className='h-[200vh] w-full bg-green-50'>
+    <main className='h-[200vh] w-full'>
       <div className='w-full'>
         <PostEditor />
       </div>
--- src/components/posts/editor/PostEditor.tsx
+++ src/components/posts/editor/PostEditor.tsx
@@ -7,6 +7,9 @@ import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 import { submitPost } from '@/actions/posts'
 import UserAvatar from '../UserAvatar'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import './styles.css'
 
 export default function PostEditor() {
   const { user } = useKindeBrowserClient()
@@ -35,9 +38,24 @@ export default function PostEditor() {
   }
 
   return (
-    <div className='flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm'>
+    <div className='flex flex-col gap-5 rounded-2xl border bg-card p-5 shadow-sm'>
       <div className='flex gap-5'>
         <UserAvatar avatarUrl={user?.picture} className='hidden sm:inline' />
+        <EditorContent
+          editor={editor}
+          className={cn(
+            'max-h-[20rem] w-full overflow-y-auto rounded-2xl bg-slate-50 px-5 py-3 dark:bg-slate-900'
+          )}
+        />
+      </div>
+      <div className='flex justify-end'>
+        <Button
+          onClick={onSubmit}
+          disabled={!input.trim()}
+          className='min-w-20'
+        >
+          Post
+        </Button>
       </div>
     </div>
   )
